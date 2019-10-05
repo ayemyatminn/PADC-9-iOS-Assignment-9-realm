7d7e1c03ecaec149a6ce1828e7a28fd656e2c731
Aye Myat Min <ayemyat.minn@mm-digital-solutions.com>
2019-10-05 14:14:51 +0630

    amm:initial commit

---
+++ Assignment-9-realm/Assignment-9-realm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment-9-realm
+//
+//  Created by Aye Myat Minn on 10/5/19.
+//  Copyright © 2019 Aye Myat Minn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
